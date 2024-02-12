4a3bf30fcf741a37476abc88de98adf460aaf39e
caio <caiothalles607@gmail.com>
2024-02-12 19:00:33 -0300

    fix(register.module): added Patch method

--- src/modules/register/infra/http/create-register.controller.ts
+++ src/modules/register/infra/http/create-register.controller.ts
@@ -1,20 +1,36 @@
 import { CreateRegisterService } from '../../services/create-register.service';
 import { CreateRegisterDTO } from '../../dto/create-register-dto';
+import { UpdateRegisterService } from '../../services/update-register.service';
+import { UpdateRegisterDTO } from '../../dto/update-register-dto';
 import {
   Body,
   Controller,
   Post,
   UsePipes,
   ValidationPipe,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  Param,
+  Patch,
 } from '@nestjs/common';
 
 @Controller('register')
-export class CreateRegisterController {
-  constructor(private readonly registerService: CreateRegisterService) {}
+export class registerController {
+  constructor(
+    private readonly registerService: CreateRegisterService,
+    private readonly updateService: UpdateRegisterService,
+  ) {}
 
   @Post()
   @UsePipes(ValidationPipe)
+  @UseInterceptors(ClassSerializerInterceptor)
   create(@Body() registerDto: CreateRegisterDTO) {
     return this.registerService.execute(registerDto);
+  }
+
+  @Patch(':id')
+  @UsePipes(ValidationPipe)
+  update(@Param('id') id: string, @Body() updateDto: UpdateRegisterDTO) {
+    return this.updateService.execute(id, updateDto);
   }
 }
