f41396d8de88f008e54375cbc7ca66c988697608
caio <caiothalles607@gmail.com>
2024-02-12 15:26:04 -0300

    fix(register-dto): changed scope structure

--- src/modules/register/dto/create-register-dto.ts
+++ src/modules/register/dto/create-register-dto.ts
@@ -1,13 +1,37 @@
 import { Crops } from '@prisma/client';
+import { IsString, IsNotEmpty, IsInt } from 'class-validator';
 
-export interface ICreateRegisterDTO {
+export class CreateRegisterDTO {
+  @IsString({ message: 'This legalId variable need to be string' })
+  @IsNotEmpty({ message: 'This legalId variable can not empty' })
   legalId: string;
+
+  @IsString({ message: 'This name variable need to be string' })
+  @IsNotEmpty({ message: 'This name variable can not empty' })
   name: string;
+
+  @IsString({ message: 'This farme_name variable need to be string' })
+  @IsNotEmpty({ message: 'This farme_name variable can not empty' })
   farm_name: string;
+
+  @IsString({ message: 'This city variable need to be string' })
+  @IsNotEmpty({ message: 'This city variable can not empty' })
   city: string;
+
+  @IsString({ message: 'This state variable need to be string' })
+  @IsNotEmpty({ message: 'This state variable can not empty' })
   state: string;
+
+  @IsInt({ message: 'This total hectares variable need to be number' })
+  @IsNotEmpty({ message: 'This total hectares variable can not empty' })
   ttl_hectares: number;
+
+  @IsInt({ message: 'This plantable area variable need to be number' })
+  @IsNotEmpty({ message: 'This plantable area variable can not empty' })
   plantable_area: number;
+
+  @IsInt({ message: 'This vegetation area variable need to be number' })
+  @IsNotEmpty({ message: 'This vegetation area variable can not empty' })
   vegetation_area: number;
   planted_crops: Crops[];
 }
