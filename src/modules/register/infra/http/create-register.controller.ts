025756385915baadbb728ac12d1b673c636f6afe
caio <caiothalles607@gmail.com>
2024-02-12 16:11:29 -0300

    fix(register.controller): add decorators

--- src/modules/register/infra/http/create-register.controller.ts
+++ src/modules/register/infra/http/create-register.controller.ts
@@ -1,12 +1,19 @@
 import { CreateRegisterService } from '../../services/create-register.service';
 import { CreateRegisterDTO } from '../../dto/create-register-dto';
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
 @Controller('register')
 export class CreateRegisterController {
   constructor(private readonly registerService: CreateRegisterService) {}
 
   @Post()
+  @UsePipes(ValidationPipe)
   create(@Body() registerDto: CreateRegisterDTO) {
     return this.registerService.execute(registerDto);
   }
