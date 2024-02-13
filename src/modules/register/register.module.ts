900650540e7dfb9db60e56a3c66588330395ab2e
caio <caiothalles607@gmail.com>
2024-02-12 22:09:38 -0300

    fix(register.module): add provider 'TotalFarms'

--- src/modules/register/register.module.ts
+++ src/modules/register/register.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { CreateRegisterService } from './services/create-register.service';
 import { UpdateRegisterService } from './services/update-register.service';
 import { DeleteRegisterService } from './services/delete-register.service';
-import { registerController } from './infra/http/register.controller';
+import { RegisterController } from './infra/http/register.controller';
+import { TotalFarms } from './services/endpoints/total-farms.service';
 
 @Module({
-  controllers: [registerController],
+  controllers: [RegisterController],
   providers: [
     CreateRegisterService,
     UpdateRegisterService,
     DeleteRegisterService,
+    TotalFarms,
   ],
 })
 export class FarmModule {}
