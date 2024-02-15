7ce5654d58e35d67775b5eadd212618af9a10e4a
caio <caiothalles607@gmail.com>
2024-02-15 09:29:28 -0300

    fix: test for the dashboard to return the use of land redone

---
+++ src/test/unit/dashboard/land-use.spec.ts
@@ -0,0 +1,20 @@
+import { LandUseFarmService } from 'src/modules/dashboard/endpoints/land-use-farm.service';
+
+describe('LandUseFarmService', () => {
+  let service: LandUseFarmService;
+
+  beforeEach(() => {
+    service = new LandUseFarmService();
+  });
+
+  it('should calculate land use percentages correctly', async () => {
+    const expectedData = {
+      usePlantable: '50.00',
+      useVegetation: '50.00',
+    };
+
+    const result = await service.execute();
+
+    expect(result).toEqual(expectedData);
+  });
+});
