bd03553a42577e685018aca68c116324e5b3c85d
caio <caiothalles607@gmail.com>
2024-02-15 10:30:47 -0300

    fix: test for the dashboard to return the total area of ​​hectares redone

--- src/test/unit/dashboard/full-area-hectares.spec.ts
+++ src/test/unit/dashboard/full-area-hectares.spec.ts
@@ -1,40 +1,17 @@
 import { FullAreaHectaresService } from 'src/modules/dashboard/endpoints/full-area-hectares.service';
 
-const mockPrismaClient = {
-  farm: {
-    aggregate: jest.fn(),
-  },
-};
-
-jest.mock('src/prisma', () => ({
-  prismaClient: mockPrismaClient,
-}));
-
 describe('FullAreaHectaresService', () => {
-  let fullAreaHectaresService: FullAreaHectaresService;
+  let service: FullAreaHectaresService;
 
   beforeEach(() => {
-    fullAreaHectaresService = new FullAreaHectaresService();
+    service = new FullAreaHectaresService();
   });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  it('should return the total hectares correctly', async () => {
+    const expectedTotalHectares = 30;
 
-  it('should return total area in hectares', async () => {
-    const mockTotalArea = {
-      _sum: {
-        ttl_hectares: 500,
-      },
-    };
+    const result = await service.execute();
 
-    mockPrismaClient.farm.aggregate.mockResolvedValueOnce(mockTotalArea);
-
-    const expectedResult = 500;
-
-    const result = await fullAreaHectaresService.execute();
-
-    expect(result).toEqual(expectedResult);
-    expect(mockPrismaClient.farm.aggregate).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(expectedTotalHectares);
   });
 });
